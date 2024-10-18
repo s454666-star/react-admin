accd85febc5c2d23e2174941fe20a9b48157d7b4
MSI\s454666 <s454666123@gmail.com>
2024-10-18 21:00:48 +0800

    user頁面

--- src/UserCreate.js
+++ src/UserCreate.js
@@ -1,18 +1,16 @@
 // src/UserCreate.js
 import React from 'react';
-import { Create, SimpleForm, TextInput, SelectInput, EmailInput, required } from 'react-admin';
+import { Create, SimpleForm, TextInput, SelectInput, required } from 'react-admin';
 
 const validateEmail = [required(), (value) => (/.+@.+\..+/.test(value) ? undefined : '請輸入有效的電子郵件')];
-const validateUsername = required('帳號為必填項');
-const validateName = required('姓名為必填項');
 
 const UserCreate = (props) => (
     <Create title="新增使用者" {...props}>
         <SimpleForm>
-            <TextInput source="username" label="帳號" validate={validateUsername} />
-            <TextInput source="password" label="密碼" type="password" validate={required()} />
-            <TextInput source="name" label="姓名" validate={validateName} />
-            <EmailInput source="email" label="電子郵件" validate={validateEmail} />
+            <TextInput source="username" label="帳號" validate={required()} />
+            <TextInput source="password" label="密碼" validate={required()} />
+            <TextInput source="name" label="姓名" validate={required()} />
+            <TextInput source="email" label="電子郵件" validate={validateEmail} />
             <TextInput source="phone" label="電話" />
             <TextInput source="address" label="地址" />
             <SelectInput source="status" label="狀態" choices={[
--- src/UserEdit.js
+++ src/UserEdit.js
@@ -1,17 +1,15 @@
 // src/UserEdit.js
 import React from 'react';
-import { Edit, SimpleForm, TextInput, SelectInput, EmailInput, required } from 'react-admin';
+import { Edit, SimpleForm, TextInput, SelectInput, required } from 'react-admin';
 
 const validateEmail = [required(), (value) => (/.+@.+\..+/.test(value) ? undefined : '請輸入有效的電子郵件')];
-const validateUsername = required('帳號為必填項');
-const validateName = required('姓名為必填項');
 
 const UserEdit = (props) => (
     <Edit title="編輯使用者" {...props}>
         <SimpleForm>
-            <TextInput source="username" label="帳號" validate={validateUsername} />
-            <TextInput source="name" label="姓名" validate={validateName} />
-            <EmailInput source="email" label="電子郵件" validate={validateEmail} />
+            <TextInput source="username" label="帳號" validate={required()} />
+            <TextInput source="name" label="姓名" validate={required()} />
+            <TextInput source="email" label="電子郵件" validate={validateEmail} />
             <TextInput source="phone" label="電話" />
             <TextInput source="address" label="地址" />
             <SelectInput source="status" label="狀態" choices={[
